66fa9a5015ebb40981f219fef24a4484ad9c930e
Gabo <gabo.salcedo.z@gmail.com>
2020-02-17 10:06:56 +0100

    a

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <section className="container">
-        <div className="row">
-          <div className="col-5 mx-auto mt-auto h-50 d-block menu--shadow">asdsad</div>
+    <section className="container menu col-5">
+      <div className="row" style={{marginTop:"30%"}}>
+        <div
+          className="col-12 menu--header mx-auto mt-auto"
+          style={{ backgroundColor: "blue", padding:"30px 0 30px 20px"}}
+        >
+     
+          Logo
         </div>
-      </section>
-    </>
+
+          
+          <div className="col-9">afghgfhgfghfsdsad</div>
+
+          
+          <nav className="col-3 nav flex-column"
+            style={{ backgroundColor: "gold" }}>
+            <a class="nav-link active" href="#">Menu</a>
+            <a class="nav-link active" href="#">Abrir</a>
+          </nav>
+
+      </div>
+    </section>
   );
 }
 
